d4f474bc1541ca8572d34ec41e49b83bbb63a0bc
olalere111 <matthew.olalere@binghamuni.edu.ng>
2025-12-19 20:03:31 +0100

    Update Igbo.py

--- Igbo.py
+++ Igbo.py
@@ -1,3 +1,28 @@
+# Yoruba to English Dictionary (20 words)
+
+yoruba_dictionary = { #by Matthew Folat
+    "ẹ̀káàrọ̀": "Good morning",
+    "ẹ̀káàsán": "Good afternoon",
+    "ẹ̀káalẹ́": "Good evening",
+    "báwo ni": "How are you?",
+    "dáadáa ni": "I am fine",
+    "orúkọ": "Name",
+    "ilé": "House",
+    "ọmọ": "Child",
+    "ọrẹ́": "Friend",
+    "ọkọ̀": "Husband",
+    "iyàwó": "Wife",
+    "ọbẹ̀": "Soup",
+    "omi": "Water",
+    "oúnjẹ": "Food",
+    "ilé-ẹ̀kọ́": "School",
+    "akẹ́kọ̀ọ́": "Student",
+    "olùkọ́": "Teacher",
+    "ìfẹ́": "Love",
+    "ọlọ́run": "God",
+    "ọjọ́": "Day"
+}
+
 
 english_to_igala = { #by Kamsi chukwumampkam
     "Hello": "Adoo",
@@ -100,6 +125,7 @@ elif choice == "2":
 
 else:
     print("Invalid choice.")
+
 
 
 
