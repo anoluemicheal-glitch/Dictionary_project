a10d9d7bbfa74c457be2af82da6ba47f135e9ca9
chukwumamkpamkamsi-debug <chukwumamkpam.kamsi@binghamuni.edu.ng>
2025-12-19 18:24:37 +0100

    Add files via upload

---
+++ igala.py
@@ -0,0 +1,23 @@
+
+english_to_igala = {
+    "Hello": "Adoo",
+    "Goodbye": "O dabo",
+    "Thank you": "Ojo",
+    "Please": "Joo",
+    "Yes": "Ehe",
+    "No": "Awo",
+    "Man": "Ono",
+    "Woman": "Iye",
+    "Child": "Oma",
+    "Food": "Eje",
+    "Water": "Omi",
+    "House": "Ile",
+    "Road": "Ona",
+    "Market": "Oja",
+    "Money": "Owo",
+    "Friend": "Ore",
+    "Father": "Ata",
+    "Mother": "Iya",
+    "Morning": "Ojo-ola",
+    "Night": "Ojo-ane"
+}
