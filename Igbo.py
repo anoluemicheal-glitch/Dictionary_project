e78bed07b78dbb95b83a750c7801560b7d542588
chukwumamkpamkamsi-debug <chukwumamkpam.kamsi@binghamuni.edu.ng>
2025-12-19 19:59:40 +0100

    Update Igbo.py

--- Igbo.py
+++ Igbo.py
@@ -1,3 +1,27 @@
+
+english_to_igala = { #by Kamsi chukwumampkam
+    "Hello": "Adoo",
+    "Goodbye": "O dabo",
+    "Thank you": "Ojo",
+    "Please": "Joo",
+    "Yes": "Ehe",
+    "No": "Awo",
+    "Man": "Ono",
+    "Woman": "Iye",
+    "Child": "Oma",
+    "Food": "Eje",
+    "Water": "Omi",
+    "House": "Ile",
+    "Road": "Ona",
+    "Market": "Oja",
+    "Money": "Owo",
+    "Friend": "Ore",
+    "Father": "Ata",
+    "Mother": "Iya",
+    "Morning": "Ojo-ola",
+    "Night": "Ojo-ane"
+}
+
 nigerian_languages = {
     "Micheal": {  # Hausa
         "Language": "Hausa",
@@ -76,5 +100,6 @@ elif choice == "2":
 
 else:
     print("Invalid choice.")
+
 
 
