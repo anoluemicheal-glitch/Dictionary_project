908ad91b2865fb9e329bab9300eab1a0da9b6832
chukwumamkpamkamsi-debug <chukwumamkpam.kamsi@binghamuni.edu.ng>
2025-12-19 19:31:52 +0100

    Update Igbo.py

--- Igbo.py
+++ Igbo.py
@@ -1,3 +1,30 @@
+nigerian_languages = {
+    "Micheal": {  # Hausa
+        "Language": "Hausa",
+        "Words": {
+            "Sannu": "Hello",
+            "Nagode": "Thank you",
+            "Eh": "Yes",
+            "A'a": "No",
+            "Ina kwana": "Good morning",
+            "Ina wuni": "Good afternoon",
+            "Ina yini": "Good evening",
+            "Lafiya": "Fine / Health",
+            "Ruwa": "Water",
+            "Abinci": "Food",
+            "Gida": "House",
+            "Mutum": "Person",
+            "Yaro": "Child",
+            "Mace": "Woman",
+            "Namiji": "Man",
+            "Kudi": "Money",
+            "Kasuwa": "Market",
+            "Hanya": "Road",
+            "Lokaci": "Time",
+            "Aiki": "Work"
+        }
+    }
+}
 # Translational Igbo ↔ English Dictionary
 
 dictionary = {
@@ -48,3 +75,4 @@ elif choice == "2":
 
 else:
     print("Invalid choice.")
+
